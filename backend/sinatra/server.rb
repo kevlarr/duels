e319c08269d753f3a42873f9e63ecfdbb8947618
kevin larrabee <larrabeat@gmail.com>
2017-09-03 16:50:17 -0400

    WIP update Sinatra - cannot install PG

--- backend/sinatra/server.rb
+++ backend/sinatra/server.rb
@@ -1,69 +1,38 @@
 require "sinatra"
 require "sinatra/activerecord"
-require "pry"
 
-["models", "seeders"].each do |dir|
+["models"].each do |dir|
   Dir["./app/#{dir}/*.rb"].each { |file| require file }
 end
 
 set :views, Proc.new { File.join(root, "app/views") }
 
-#HTTP Views
 get "/" do
-  @books = Book.order(:title)
   erb :index
-end
-
-get "/book/:id" do
-  @book = Book.find(params[:id].to_i)
-  erb :books_show
-end
-
-post "/book/:id/reviews/new" do
-  book = Book.find(params[:id].to_i)
-  Review.create(
-    book: book,
-    score: params[:review_score].to_i,
-    description: params[:review_description]
-  )
-  redirect to("/book/#{book.id}")
-end
-
-get "/books/new" do
-  erb :books_new
-end
-
-post "/books/new" do
-  Book.create(
-    title: params[:title],
-    author: params[:author],
-    description: params[:description]
-  )
-  redirect to("/")
-end
-
-#API endpoints
-get "/api/v1/book/:id" do
-  content_type :json
-  @book = Book.find(params[:id].to_i)
-  {
-    book: @book,
-    reviews: @book.reviews
-  }.to_json
 end
 
 get "/api/v1/books" do
   content_type :json
-  @books = Book.order(:title)
-  books = []
-  @books.each do |book|
-    books << {
+
+  books = Book.order(:title).map do |book|
+    {
       book: book,
       review_score: book.average_review_score,
       reviews: book.reviews
     }
   end
+
   books.to_json
+end
+
+get "/api/v1/books/:id" do
+  content_type :json
+  book = Book.find(params[:id].to_i)
+
+  {
+    book: book,
+    reviews: book.reviews
+  }.to_json
 end
 
 post "/api/v1/books/new" do
@@ -72,10 +41,13 @@ post "/api/v1/books/new" do
     author: params[:author],
     description: params[:description]
   )
+
   if book.valid?
     status 200
+    { book: book.to_json }
   else
     status 422
+    { error: "Book could not be created" }
   end
 end
 
