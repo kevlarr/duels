94e02cf239b8fac9ec042fa23ca997929be0044a
kevin larrabee <larrabeat@gmail.com>
2017-09-10 21:55:04 -0400

    Clean up Sinatra and update build file

--- backend/sinatra/app/app/models/book.rb
+++ backend/sinatra/app/app/models/book.rb
@@ -4,12 +4,4 @@ class Book < ActiveRecord::Base
   validates :title, presence: true
   validates :author, presence: true
   validates :description, presence: true
-
-  def average_review_score
-    if reviews.any?
-      "Average Rating: #{(reviews.map(&:score).sum / reviews.count).to_f.round(1)}"
-    else
-      "No Ratings"
-    end
-  end
 end
--- backend/sinatra/app/server.rb
+++ backend/sinatra/app/server.rb
@@ -2,10 +2,9 @@ require "bundler/setup"
 require "sinatra"
 require "sinatra/activerecord"
 
-["models"].each do |dir|
-  Dir["./app/#{dir}/*.rb"].each { |file| require file }
-end
+Dir["./app/models/*.rb"].each { |file| require file }
 
+set :bind, '0.0.0.0'
 set :views, Proc.new { File.join(root, "app/views") }
 
 get "/" do
@@ -15,27 +14,20 @@ end
 get "/api/v1/books" do
   content_type :json
 
-  books = Book.order(:title).map do |book|
-    {
-      book: book,
-      review_score: book.average_review_score
-    }
-  end
-
-  books.to_json
+  status 200
+  {books: Book.order(:title)}.to_json
 end
 
-get "/api/v1/books/:id" do
+get "/api/v1/books/:book" do
   content_type :json
-  book = Book.find(params[:id].to_i)
+  book = Book.find(params[:book].to_i)
 
-  {
-    book: book,
-    reviews: book.reviews
-  }.to_json
+  status 200
+  {book: book, reviews: book.reviews}.to_json
 end
 
 post "/api/v1/books/new" do
+  content_type :json
   book = Book.create(
     title: params[:title],
     author: params[:author],
@@ -44,23 +36,27 @@ post "/api/v1/books/new" do
 
   if book.valid?
     status 200
-    { book: book.to_json }
+    {book: book.to_json}
   else
     status 422
-    { error: "Book could not be created" }
+    {error: "Book could not be created"}
   end
 end
 
-post "/api/v1/book/:id/reviews/new" do
-  book = Book.find(params[:id].to_i)
+post "/api/v1/books/:book/reviews/new" do
+  content_type :json
+  book = Book.find(params[:book].to_i)
   review = Review.create(
     book: book,
     score: params[:review_score].to_i,
     description: params[:review_description]
   )
+
   if review.valid?
     status 200
+    {review: review.to_json}
   else
     status 422
+    {error: "Review could not be created"}
   end
 end
